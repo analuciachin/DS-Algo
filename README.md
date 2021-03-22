979ceb9ff65051cc3d31111b7714d15cccc3ca22
analuciachin <ananime@gmail.com>
2021-03-22 18:20:53 +0000

    implement twoSum function and update README.md file

---
+++ README.md
@@ -0,0 +1,12 @@
+# Data Structure and Algorithms
+
+## Data Structure
+
+- Arrays
+  - lookup (look for an element in an array): O(1)
+  - push (add an element at the end of an array): O(1)
+  - pop (remove the last item of an array): O(1)
+  - unshift (add an item at the beginning of an array): O(n)
+  - splice (add/delete items in the middle of an array): O(n)
+  - insert: O(n)
+  - delete: O(n)
---
+++ arrays/two_sum.js
@@ -0,0 +1,28 @@
+// Given an array of integers nums and an integer target, return the indices of the two numbers such that they add up to a target
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice
+
+// inputs: nums=[2,7,11,15], target=9
+// output: [0,1] => because 2+7 = 9
+
+// inputs: nums=[3,2,4], target=6
+// output: [1,2]
+
+// inputs: nums=[3,3], target=6
+// output: [0,1]
+
+const twoSum = (nums, target) => {
+  const result = [];
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        result.push(i, j);
+      }
+    }
+  }
+  return result;
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 3], 6));
