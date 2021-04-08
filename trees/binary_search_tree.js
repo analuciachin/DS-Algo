5ea39634dd51841818a9a2f882a31cb0acb03e37
analuciachin <ananime@gmail.com>
2021-04-08 18:08:59 +0000

    andrei solution of insert method

--- trees/binary_search_tree.js
+++ trees/binary_search_tree.js
@@ -10,26 +10,34 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  // Andrei solution
   insert(value) {
     const newNode = new Node(value);
     if (this.root === null) {
       this.root = newNode;
     } else {
       let currentNode = this.root;
-      while (currentNode.left !== null && currentNode.right !== null) {
-        if (currentNode.value < value) {
-          currentNode = currentNode.right;
-        } else {
+      while (true) {
+        if (value < currentNode.value) {
+          // left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
           currentNode = currentNode.left;
+        } else {
+          // right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
         }
       }
-
-      if (currentNode.value < value) {
-        currentNode.right = newNode;
-      } else currentNode.left = newNode;
     }
-    return this;
   }
+
   lookup(value) {
     if (this.root === null) {
       return null;
@@ -61,7 +69,6 @@ tree.insert(4);
 tree.insert(1);
 tree.insert(6);
 tree.insert(20);
-
 tree.insert(15);
 tree.insert(170);
 console.log(JSON.stringify(traverse(tree.root)));
